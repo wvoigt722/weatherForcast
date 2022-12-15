7f78bb730531514730ee712f4ac59d0a22659296
William Voigt <wvoigt@Williams-MacBook-Air.local>
2022-12-14 19:13:12 -0500

    added api to swap city to lat/ long

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+searchMainBtnEl.addEventListener('click', function() {
+    console.log(searchMainEl.value);
+    
+    
+    var city = searchMainEl.value;
+    
+    
+    
+  var requestOptions = {
+    method: 'GET',
+  };
+  
+  fetch(
+    
+    'https://api.geoapify.com/v1/geocode/search?text=' + city + '&apiKey=51181722fe3645919bd3cf77a3c846e9', 
+    
+  
+  requestOptions)
+  .then(response => response.json())
+  .then(result => { 
+    
+    console.log(result);
+    
+    console.log(result.features[0].bbox[0]);
+    console.log(result.features[0].bbox[1]);
+    
+    // eventually to be stored in main lat and long vars
+    latitude = result.features[0].bbox[1];
+    longitude = result.features[0].bbox[0];
+    
+    getInfo();
+  
+  
+  })
+  
+  .catch(error => console.log('error', error));
+  
+  
+  });
